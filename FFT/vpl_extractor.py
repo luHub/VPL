16063caf0ca349929ac5a389029fa33157c0b8e9
luHub <lucioguerchi16f@gmail.com>
2025-10-03 00:07:29 +0200

    Update vpl_extractor.py

--- FFT/vpl_extractor.py
+++ FFT/vpl_extractor.py
@@ -1,134 +1,106 @@
 import json
-from pycparser import c_parser, c_ast
+from pycparser import c_parser, c_ast, parse_file
 
-# -------------------------------
-# VPL JSON IR Generator
-# -------------------------------
-
-class VPLIRGenerator(c_ast.NodeVisitor):
+class VPLExtractor(c_ast.NodeVisitor):
     def __init__(self):
-        self.ir = {"functions": [], "variables": {}, "operations": []}
-        self.current_function = None
+        self.ir = {"functions": []}
 
     def visit_FuncDef(self, node):
         func_name = node.decl.name
-        self.current_function = {
+        #params = [p.name for p in node.decl.type.args.params]
+        params = []
+        body = []
+        self._visit_block(node.body.block_items, body)
+        self.ir["functions"].append({
             "name": func_name,
-            "params": [],
-            "body": []
-        }
-        # Process parameters
-        if isinstance(node.decl.type, c_ast.FuncDecl) and node.decl.type.args:
-            for param in node.decl.type.args.params:
-                if isinstance(param, c_ast.Decl):
-                    self.current_function["params"].append(param.name)
+            "params": params,
+            "body": body
+        })
 
-        # Process function body
-        self.visit(node.body)
+    def _visit_block(self, block_items, body):
+        if block_items is None:
+            return
+        for stmt in block_items:
+            self._visit_stmt(stmt, body)
 
-        self.ir["functions"].append(self.current_function)
-        self.current_function = None
-
-    def visit_Compound(self, node):
-        for stmt in node.block_items or []:
-            self.visit(stmt)
-
-    def visit_Assignment(self, node):
-        target = self.get_name(node.lvalue)
-        expr = self.get_expr(node.rvalue)
-        op = {
-            "type": "assignment",
-            "target": target,
-            "value": expr
-        }
-        self.ir["operations"].append(op)
-        if self.current_function is not None:
-            self.current_function["body"].append(op)
-
-    def visit_If(self, node):
-        cond = self.get_expr(node.cond)
-        if_op = {"type": "if", "condition": cond, "then": [], "else": []}
-        if node.iftrue:
-            prev_ops = self.ir["operations"]
-            self.ir["operations"] = []
-            self.visit(node.iftrue)
-            if_op["then"] = self.ir["operations"]
-            self.ir["operations"] = prev_ops
-        if node.iffalse:
-            prev_ops = self.ir["operations"]
-            self.ir["operations"] = []
-            self.visit(node.iffalse)
-            if_op["else"] = self.ir["operations"]
-            self.ir["operations"] = prev_ops
-        self.ir["operations"].append(if_op)
-        if self.current_function is not None:
-            self.current_function["body"].append(if_op)
-
-    def visit_While(self, node):
-        cond = self.get_expr(node.cond)
-        while_op = {"type": "while", "condition": cond, "body": []}
-        prev_ops = self.ir["operations"]
-        self.ir["operations"] = []
-        self.visit(node.stmt)
-        while_op["body"] = self.ir["operations"]
-        self.ir["operations"] = prev_ops
-        self.ir["operations"].append(while_op)
-        if self.current_function is not None:
-            self.current_function["body"].append(while_op)
-
-    def get_name(self, node):
-        if isinstance(node, c_ast.ID):
-            return node.name
-        return str(node)
-
-    def get_expr(self, node):
-        if isinstance(node, c_ast.Constant):
-            return {"type": "const", "value": node.value}
-        elif isinstance(node, c_ast.ID):
-            return {"type": "var", "name": node.name}
-        elif isinstance(node, c_ast.BinaryOp):
-            return {
-                "type": "binop",
-                "op": node.op,
-                "left": self.get_expr(node.left),
-                "right": self.get_expr(node.right)
+    def _visit_stmt(self, stmt, body):
+        if isinstance(stmt, c_ast.Decl):
+            body.append({"decl": stmt.name})
+        elif isinstance(stmt, c_ast.Assignment):
+            body.append({
+                "assign": {
+                    "lvalue": self._expr(stmt.lvalue),
+                    "rvalue": self._expr(stmt.rvalue)
+                }
+            })
+        elif isinstance(stmt, c_ast.FuncCall):
+            body.append({
+                "call": {
+                    "func": self._expr(stmt.name),
+                    "args": [self._expr(arg) for arg in stmt.args.exprs]
+                }
+            })
+        elif isinstance(stmt, c_ast.For):
+            loop = {
+                "for": {
+                    "init": self._expr(stmt.init),
+                    "cond": self._expr(stmt.cond),
+                    "next": self._expr(stmt.next),
+                    "body": []
+                }
             }
-        elif isinstance(node, c_ast.UnaryOp):
+            self._visit_block(stmt.stmt.block_items, loop["for"]["body"])
+            body.append(loop)
+        elif isinstance(stmt, c_ast.Compound):
+            self._visit_block(stmt.block_items, body)
+        else:
+            body.append({"stmt": type(stmt).__name__})
+
+    def _expr(self, expr):
+        if expr is None:
+            return None
+        if isinstance(expr, c_ast.Constant):
+            return {"const": expr.value}
+        elif isinstance(expr, c_ast.ID):
+            return {"id": expr.name}
+        elif isinstance(expr, c_ast.BinaryOp):
             return {
-                "type": "unary",
-                "op": node.op,
-                "expr": self.get_expr(node.expr)
+                "binop": {
+                    "op": expr.op,
+                    "left": self._expr(expr.left),
+                    "right": self._expr(expr.right)
+                }
             }
-        elif isinstance(node, c_ast.FuncCall):
+        elif isinstance(expr, c_ast.FuncCall):
             return {
-                "type": "call",
-                "func": self.get_name(node.name),
-                "args": [self.get_expr(arg) for arg in node.args.exprs] if node.args else []
+                "call": {
+                    "func": self._expr(expr.name),
+                    "args": [self._expr(arg) for arg in expr.args.exprs]
+                }
             }
-        return {"type": "unknown", "repr": str(node)}
+        elif isinstance(expr, c_ast.UnaryOp):
+            return {"unop": {"op": expr.op, "expr": self._expr(expr.expr)}}
+        elif isinstance(expr, c_ast.ID):
+            return {"id": expr.name}
+        elif isinstance(expr, c_ast.StructRef):
+            return {
+                "structref": {
+                    "name": self._expr(expr.name),
+                    "field": expr.field.name
+                }
+            }
+        return {"expr": type(expr).__name__}
 
-# -------------------------------
-# Example: Parse C code
-# -------------------------------
+# ---------------------------
+# Run extractor
+# ---------------------------
+if __name__ == "__main__":
+    parser = c_parser.CParser()
+    with open("fft_cooley_tukey.c", "r") as f:
+        code = f.read()
+    ast = parser.parse(code)
 
-c_code = r"""
-int main() {
-    int X;
-    X = 0;
-    while (X < 5) {
-        X = X + 2;
-    }
-    return X;
-}
-"""
+    extractor = VPLExtractor()
+    extractor.visit(ast)
 
-parser = c_parser.CParser()
-ast = parser.parse(c_code)
-
-# Generate IR
-generator = VPLIRGenerator()
-generator.visit(ast)
-
-# Dump JSON IR
-json_ir = json.dumps(generator.ir, indent=2)
-print(json_ir)
+    print(json.dumps(extractor.ir, indent=2))
